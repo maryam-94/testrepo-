f0c99a4a66eb2b4ae490384c3a2de9eff5a95af5
maryam-94 <58740517+maryam-94@users.noreply.github.com>
2021-10-27 18:25:02 +0200

    Add files via upload

---
+++ pythonExercices.py
@@ -0,0 +1,38 @@
+
+
+# -*- coding: utf8 -*-
+
+
+quotes = [
+    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
+    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
+]
+
+characters = [
+    "alvin et les Chipmunks", 
+    "Babar", 
+    "betty boop", 
+    "calimero", 
+    "casper", 
+    "le chat potté", 
+    "Kirikou"
+]
+
+
+ import turtle
+ turtle.forward(100)
+
+ from turtle import *
+color('red', 'yellow')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
+
+
+
+
